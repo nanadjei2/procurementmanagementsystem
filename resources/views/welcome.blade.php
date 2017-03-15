80ad3edbd2adc9acd9545b38b211912d0bb4ff43
unknown <possible-kwaku>
2017-03-15 11:47:33 +0000

    added changes to index file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Procurement Management system</div>
             </div>
         </div>
     </body>
