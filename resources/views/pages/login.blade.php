a92961ab309894c170c22bb8302ae5e6a0c524f3
nanadjei2 <nana.elvee@gmail.com>
2017-03-15 07:30:54 -0700

    Login page with Navbar created

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,14 @@
+@extends("master")
+@section("title")
+    PMS | Login
+@endsection
+
+@section("content")
+    <section>
+        <div class="container">
+            <div>
+                <h2>You are on the login page</h2>
+            </div>
+        </div>
+    </section>
+@endsection 
