cd57ed066254e634fc9feb2ba6696d9b0b947789
nanadjei2 <nana.elvee@gmail.com>
2017-03-15 08:00:57 -0700

    Login form created

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -6,8 +6,21 @@
 @section("content")
     <section>
         <div class="container">
-            <div>
-                <h2>You are on the login page</h2>
+            <div class="col-md-4 col-md-offset-4 col-xs-12">
+                <div class="login-form">
+                <h3>Please Login</h3>
+                    <div class="form-group">
+                        <label for="username"> Username:</label>
+                        <input id="username" class="form-control" type="text" placeholder="Enter username" required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd"> Password:</label>
+                        <input id="pwd" class="form-control" type="text" placeholder="Enter password" required/>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
