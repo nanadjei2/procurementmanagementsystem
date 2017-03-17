7e16fca27ed5b523c22b6c9a034a162a7ba13f95
unknown <possible-kwaku>
2017-03-17 08:36:26 +0000

    changes added to migration tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('0');
             $table->rememberToken();
             $table->timestamps();
         });
