cb69cbe86d62cb00dcbabec2bf293c651f2fd9a0
nanadjei2 <nana.elvee@gmail.com>
2017-03-20 12:20:41 -0700

    Dashboard icons displayed

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -9,7 +9,179 @@
         <div class="container">
             @include('common.sweet-notifications')
             
-                <h2>You Are On The Dashboard Page</h2>
+
+
+
+
+            <div class="row m-b-40">
+    <div class="col-xs-12 col-lg-4 m-b-5">
+        <div class="text-widget-1">
+            <div class="row">
+                <div class="col-xs-4">
+                    <span class="fa-stack fa-stack-2x pull-left">
+<i class="fa fa-circle fa-stack-2x color-success"></i>
+<i class="fa fa fa-usd fa-stack-1x color-white"></i>
+</span> 
+                </div>
+                <div class="col-xs-8 text-left">
+                    <div class="row">
+                        <a class="col-xs-12">
+                            <div class="title">My Profile</div>
+                        </a>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="numbers">
+                                <span class="amount">$1,123</span> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-lg-4 m-b-5">
+        <div class="text-widget-1">
+            <div class="row">
+                <div class="col-xs-4">
+                    <span class="fa-stack fa-stack-2x pull-left">
+<i class="fa fa-circle fa-stack-2x color-danger"></i>
+<i class="fa fa fa-btc fa-stack-1x color-white"></i>
+</span> 
+                </div>
+                <div class="col-xs-8 text-left">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="title">Add Supplier or Contractor Information</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="numbers">
+                                <span class="amount">$863</span> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-lg-4 m-b-5">
+        <div class="text-widget-1">
+            <div class="row">
+                <div class="col-xs-4">
+                    <span class="fa-stack fa-stack-2x pull-left">
+<i class="fa fa-circle fa-stack-2x color-warning"></i>
+<i class="fa fa fa-eur fa-stack-1x color-white"></i>
+</span> 
+                </div>
+                <div class="col-xs-8 text-left">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="title">View</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="numbers">
+                                <span class="amount">$3,863</span> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="row m-b-40">
+    <div class="col-xs-12 col-lg-4 m-b-5">
+        <div class="text-widget-1">
+            <div class="row">
+                <div class="col-xs-4">
+                    <span class="fa-stack fa-stack-2x pull-left">
+<i class="fa fa-circle fa-stack-2x color-success"></i>
+<i class="fa fa fa-usd fa-stack-1x color-white"></i>
+</span> 
+                </div>
+                <div class="col-xs-8 text-left">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="title">Update</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="numbers">
+                                <span class="amount">$1,123</span> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-lg-4 m-b-5">
+        <div class="text-widget-1">
+            <div class="row">
+                <div class="col-xs-4">
+                    <span class="fa-stack fa-stack-2x pull-left">
+<i class="fa fa-circle fa-stack-2x color-danger"></i>
+<i class="fa fa fa-btc fa-stack-1x color-white"></i>
+</span> 
+                </div>
+                <div class="col-xs-8 text-left">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="title">Revenue yesterday</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="numbers">
+                                <span class="amount">$863</span> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-lg-4 m-b-5">
+        <div class="text-widget-1">
+            <div class="row">
+                <div class="col-xs-4">
+                    <span class="fa-stack fa-stack-2x pull-left">
+<i class="fa fa-circle fa-stack-2x color-warning"></i>
+<i class="fa fa fa-eur fa-stack-1x color-white"></i>
+</span> 
+                </div>
+                <div class="col-xs-8 text-left">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="title">logout</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="numbers">
+                                <span class="amount">$3,863</span> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
             </div>
         </div><!--Container-->
     </section>
