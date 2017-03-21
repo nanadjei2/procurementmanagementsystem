ed50645e2f725c444e885c74901a6fd7867ba044
nanadjei2 <nana.elvee@gmail.com>
2017-03-21 04:26:30 -0700

    dashboard totally done

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -8,138 +8,130 @@
         
         <div class="container">
             @include('common.sweet-notifications')
-            
-
-
-
-
-            <div class="row m-b-40">
-    <a href="#" class="col-xs-12 col-lg-4 m-b-5">
-        <div class="text-widget-1">
-            <div class="row">
-                <div class="col-xs-4">
-                    <span class="fa-stack fa-stack-2x pull-left">
-<i class="fa fa-circle fa-stack-2x color-info"></i>
-<i class="fa fa fa-usd fa-stack-1x color-white"></i>
-</span> 
-                </div>
-                <div class="col-xs-8 text-left">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="amount">MY PROFILE</div>
+                <div class="dashboard-activities">
+                        <div class="row m-b-40">
+                            <a href="#" class="col-xs-12 col-lg-4 m-b-5">
+                                <div class="text-widget-1">
+                                    <div class="row">
+                                        <div class="col-xs-4">
+                                            <span class="fa-stack fa-stack-2x pull-left">
+                        <i class="fa fa-circle fa-stack-2x color-info"></i>
+                        <i class="fa fa fa-user fa-stack-1x color-white"></i>
+                        </span> 
+                                        </div>
+                                        <div class="col-xs-8 text-left">
+                                            <div class="row">
+                                                <div class="col-xs-12">
+                                                    <div class="amount my-profile">MY PROFILE</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="#" class="col-xs-12 col-lg-4 m-b-5">
+                                <div class="text-widget-1">
+                                    <div class="row">
+                                        <div class="col-xs-4">
+                                            <span class="fa-stack fa-stack-2x pull-left">
+                        <i class="fa fa-circle fa-stack-2x color-success"></i>
+                        <i class="fa fa fa-user-plus fa-stack-1x color-white"></i>
+                        </span> 
+                                        </div>
+                                        <div class="col-xs-8 text-left">
+                                            <div class="row">
+                                                <div href="#" class="col-xs-12">
+                                                    <div class="amount">ADD SUPPLIER OR CONTRACTOR INFORMATION</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="#" class="col-xs-12 col-lg-4 m-b-5">
+                                <div class="text-widget-1">
+                                    <div class="row">
+                                        <div class="col-xs-4">
+                                            <span class="fa-stack fa-stack-2x pull-left">
+                        <i class="fa fa-circle fa-stack-2x color-warning"></i>
+                        <i class="fa fa fa-eye fa-stack-1x color-white"></i>
+                        </span> 
+                                        </div>
+                                        <div class="col-xs-8 text-left">
+                                            <div class="row">
+                                                <div class="col-xs-12 vie">
+                                                    <div class="amount view">VIEW</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a>
-    <a href="#" class="col-xs-12 col-lg-4 m-b-5">
-        <div class="text-widget-1">
-            <div class="row">
-                <div class="col-xs-4">
-                    <span class="fa-stack fa-stack-2x pull-left">
-<i class="fa fa-circle fa-stack-2x color-success"></i>
-<i class="fa fa fa-btc fa-stack-1x color-white"></i>
-</span> 
-                </div>
-                <div class="col-xs-8 text-left">
-                    <div class="row">
-                        <div href="#" class="col-xs-12">
-                            <div class="amount">ADD SUPPLIER OR CONTRACTOR INFORMATION</div>
+
+
+
+                        <div class="row m-b-40">
+                            <a href="#" class="col-xs-12 col-lg-4 m-b-5">
+                                <div class="text-widget-1">
+                                    <div class="row">
+                                        <div class="col-xs-4">
+                                            <span class="fa-stack fa-stack-2x pull-left">
+                        <i class="fa fa-circle fa-stack-2x color-success"></i>
+                        <i class="fa fa fa-pencil fa-stack-1x color-white"></i>
+                        </span> 
+                                        </div>
+                                        <div class="col-xs-8 text-left">
+                                            <div class="row">
+                                                <div class="col-xs-12">
+                                                    <div class="amount update">UPDATE</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="#" class="col-xs-12 col-lg-4 m-b-5">
+                                <div class="text-widget-1">
+                                    <div class="row">
+                                        <div class="col-xs-4">
+                                            <span class="fa-stack fa-stack-2x pull-left">
+                        <i class="fa fa-circle fa-stack-2x color-primary"></i>
+                        <i class="fa fa fa-btc fa-stack-1x color-white"></i>
+                        </span> 
+                                        </div>
+                                        <div class="col-xs-8 text-left">
+                                            <div class="row">
+                                                <div class="col-xs-12">
+                                                    <div class="amount something-else">SOMETHING ELSE</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="{{ route('logout') }}" class="col-xs-12 col-lg-4 m-b-5">
+                                <div class="text-widget-1">
+                                    <div class="row">
+                                        <div class="col-xs-4">
+                                            <span class="fa-stack fa-stack-2x pull-left">
+                        <i class="fa fa-circle fa-stack-2x color-danger"></i>
+                        <i class="fa fa fa-sign-out fa-stack-1x color-white"></i>
+                        </span> 
+                                        </div>
+                                        <div class="col-xs-8 text-left">
+                                            <div class="row">
+                                                <div class="col-xs-12">
+                                                    <div class="amount logout">LOGOUT</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                    </div>
                 </div>
-            </div>
-        </div>
-    </a>
-    <a href="#" class="col-xs-12 col-lg-4 m-b-5">
-        <div class="text-widget-1">
-            <div class="row">
-                <div class="col-xs-4">
-                    <span class="fa-stack fa-stack-2x pull-left">
-<i class="fa fa-circle fa-stack-2x color-warning"></i>
-<i class="fa fa fa-eur fa-stack-1x color-white"></i>
-</span> 
-                </div>
-                <div class="col-xs-8 text-left">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="amount">VIEW</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a>
-</div>
 
-
-
-<div class="row m-b-40">
-    <a href="#" class="col-xs-12 col-lg-4 m-b-5">
-        <div class="text-widget-1">
-            <div class="row">
-                <div class="col-xs-4">
-                    <span class="fa-stack fa-stack-2x pull-left">
-<i class="fa fa-circle fa-stack-2x color-success"></i>
-<i class="fa fa fa-usd fa-stack-1x color-white"></i>
-</span> 
-                </div>
-                <div class="col-xs-8 text-left">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="amount">Update</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a>
-    <a href="#" class="col-xs-12 col-lg-4 m-b-5">
-        <div class="text-widget-1">
-            <div class="row">
-                <div class="col-xs-4">
-                    <span class="fa-stack fa-stack-2x pull-left">
-<i class="fa fa-circle fa-stack-2x color-danger"></i>
-<i class="fa fa fa-btc fa-stack-1x color-white"></i>
-</span> 
-                </div>
-                <div class="col-xs-8 text-left">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="amount">Revenue yesterday</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a>
-    <a href="#" class="col-xs-12 col-lg-4 m-b-5">
-        <div class="text-widget-1">
-            <div class="row">
-                <div class="col-xs-4">
-                    <span class="fa-stack fa-stack-2x pull-left">
-<i class="fa fa-circle fa-stack-2x color-warning"></i>
-<i class="fa fa fa-eur fa-stack-1x color-white"></i>
-</span> 
-                </div>
-                <div class="col-xs-8 text-left">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="amount">logout</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </a>
-</div>
-
-
-
-
-
-            </div>
         </div><!--Container-->
     </section>
 @endsection 
